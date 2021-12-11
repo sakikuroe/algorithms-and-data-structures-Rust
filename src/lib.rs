3f750d46a009adc3626f0d4f632aba4d1d80ecef
sakikuroe <sakikuroe96@gmail.com>
2021-12-11 12:47:52 +0900

    Fix indentation by rust-fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ mod union_find;
 
 #[cfg(test)]
 mod tests {
-        #[test]
-        fn it_works() {
-                let result = 2 + 2;
-                assert_eq!(result, 4);
-        }
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
 }
