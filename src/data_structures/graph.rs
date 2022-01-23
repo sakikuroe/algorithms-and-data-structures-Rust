8a446a7c3c058638c63c59cbcb49481646946797
sakikuroe <sakikuroe96@gmail.com>
2022-01-24 07:39:25 +0900

    Add method

--- src/data_structures/graph.rs
+++ src/data_structures/graph.rs
@@ -1,53 +1,250 @@
-use std::{collections::HashSet, fmt};
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+    fmt,
+};
 
-pub trait IntoEdge {
-    fn into_edge(self) -> Edge;
+#[derive(Eq, PartialEq)]
+pub struct Node {
+    pub vertex: usize,
+    pub priory: usize,
 }
 
-impl IntoEdge for (usize, usize) {
-    fn into_edge(self) -> Edge {
-        Edge {
-            src: self.0,
-            dst: self.1,
-            weight: 1,
-        }
-    }
-}
-impl IntoEdge for (usize, usize, usize) {
-    fn into_edge(self) -> Edge {
-        Edge {
-            src: self.0,
-            dst: self.1,
-            weight: self.2,
-        }
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.priory.cmp(&other.priory).reverse()
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Edge {
     pub src: usize,
     pub dst: usize,
     pub weight: usize,
 }
 
+impl Ord for Edge {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.weight.cmp(&other.weight)
+    }
+}
+
+impl PartialOrd for Edge {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Edge {
+    pub fn new(src: usize, dst: usize, weight: usize) -> Edge {
+        Edge {
+            src,
+            dst,
+            weight,
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Graph {
-    pub size: usize,
-    pub edges: Vec<HashSet<Edge>>,
+    size: usize,
+    pub edges: Vec<HashMap<usize, Edge>>,
 }
 
 impl Graph {
-    pub fn new(n: usize) -> Self {
+    pub fn new(size: usize) -> Self {
         Graph {
-            size: n,
-            edges: vec![HashSet::new(); n],
+            size,
+            edges: vec![HashMap::new(); size],
         }
     }
-    pub fn add_edge(&mut self, src: usize, dst: usize) {
-        self.edges[src].insert((src, dst).into_edge());
+
+    pub fn size(&self) -> usize {
+        self.size
     }
-    pub fn add_weighted_edge(&mut self, src: usize, dst: usize, weight: usize) {
-        self.edges[src].insert((src, dst, weight).into_edge());
+
+    pub fn add_edge(&mut self, src: usize, dst: usize, weight: usize) {
+        self.edges[src].insert(dst, Edge::new(src, dst, weight));
+    }
+
+    pub fn contains_edge(&self, src: usize, dst: usize) -> bool {
+        self.edges[src].contains_key(&dst)
+    }
+
+    pub fn connected(&self, src: usize, dst: usize) -> bool {
+        self.edges[src].contains_key(&dst)
+        || self.edges[dst].contains_key(&src)
+    }
+
+    pub fn add_undirected_edge(&mut self, src: usize, dst: usize, weight: usize) {
+        self.add_edge(src, dst, weight);
+        self.add_edge(dst, src, weight);
+    }
+
+    pub fn get_diameter(&self) -> Option<usize> {
+        if !self.is_tree() {
+            return None;
+        }
+        fn arg_max(v: &Vec<usize>) -> usize {
+            v.into_iter()
+                .enumerate()
+                .fold((0, 0), |(i, max), (j, &x)| if max < x { (j, x) } else { (i, max) })
+                .0
+        }
+        let new_src = arg_max(&self.gen_undirected_graph().dijkstra(0));
+        Some(
+            self.gen_undirected_graph()
+                .dijkstra(new_src)
+                .into_iter()
+                .fold(0, |x, y| std::cmp::max(x, y)),
+        )
+    }
+
+    pub fn get_all_edges(&self) -> HashSet<Edge> {
+        let mut res = HashSet::new();
+        for e in &self.edges {
+            res = res
+                .union(&e.values().into_iter().map(|x| *x).collect::<HashSet<_>>())
+                .into_iter()
+                .map(|x| *x)
+                .collect::<HashSet<Edge>>();
+        }
+        res
+    }
+
+    pub fn gen_undirected_graph(&self) -> Graph {
+        let mut g = Graph::new(self.size);
+        for edges in &self.edges {
+            for (_, &e) in edges {
+                g.add_undirected_edge(e.src, e.dst, e.weight);
+            }
+        }
+        g
+    }
+
+    pub fn get_connected_components(&self) -> usize {
+        let mut visited = vec![false; self.size];
+        let g = self.gen_undirected_graph();
+        let mut res = 0;
+        for i in 0..g.size {
+            if !visited[i] {
+                let mut que = VecDeque::new();
+                que.push_back(i);
+
+                while let Some(node) = que.pop_front() {
+                    visited[node] = true;
+                    for &e in g.edges[node].values() {
+                        if !visited[e.dst] {
+                            que.push_back(e.dst);
+                        }
+                    }
+                }
+                res += 1;
+            }
+        }
+        res
+    }
+
+    pub fn is_tree(&self) -> bool {
+        if self.get_connected_components() != 1 {
+            return false;
+        }
+        let g = self.gen_undirected_graph();
+        if g.get_all_edges().len() == 2 * (g.size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    pub fn is_bipartite(&self) -> Option<(usize, usize)> {
+        let mut que = VecDeque::new();
+        if self.size == 0 {
+            return Some((0, 0));
+        }
+
+        let mut color = vec![None; self.size];
+        que.push_back((0, 0));
+
+        let (mut b, mut w) = (0, 0);
+        while let Some((v, p)) = que.pop_front() {
+            if p == 0 {
+                w += 1;
+            } else {
+                b += 1;
+            }
+            color[v] = Some(p ^ 1);
+            for (&dst, _e) in &self.edges[v] {
+                if color[dst] == Some(p ^ 1) {
+                    return None;
+                }
+                if color[dst].is_none() {
+                    que.push_back((dst, p ^ 1));
+                }
+            }
+        }
+
+        if b + w == self.size {
+            return Some((b, w));
+        } else {
+            return None;
+        }
+    }
+
+    pub fn topological_sort(&self) -> Result<Vec<usize>, &str> {
+        let mut indegree = {
+            let mut res = vec![0; self.size];
+            for edge_list in &self.edges {
+                for (_, &e) in edge_list {
+                    res[e.dst] += 1;
+                }
+            }
+            res
+        };
+        let res = {
+            let mut sorted: Vec<usize> = vec![];
+            let mut que = {
+                let mut que = BinaryHeap::new();
+                for i in 0..self.size {
+                    if indegree[i] == 0 {
+                        que.push(Node {
+                            priory: i,
+                            vertex: i,
+                        });
+                    }
+                }
+                que
+            };
+            while let Some(Node {
+                priory,
+                vertex: _,
+            }) = que.pop()
+            {
+                for (_, &e) in &self.edges[priory] {
+                    indegree[e.dst] -= 1;
+                    if indegree[e.dst] == 0 {
+                        que.push(Node {
+                            priory: e.dst,
+                            vertex: e.dst,
+                        })
+                    }
+                }
+                sorted.push(priory);
+            }
+
+            if sorted.len() == self.size {
+                Ok(sorted)
+            } else {
+                Err("-1")
+            }
+        };
+        res
     }
 }
 
@@ -56,14 +253,18 @@ impl fmt::Display for Graph {
         let g = (*self).clone();
         writeln!(f, "Size : {:?}", g.size)?;
         writeln!(f, "Edges: {{",)?;
-        for i in 0..g.size {
-            let mut edges = g.edges[i].clone().into_iter().collect::<Vec<Edge>>();
+        for edges in g.edges {
+            let mut edges = edges.values().map(|x| *x).collect::<Vec<_>>();
             edges.sort();
-            for &e in &edges {
-                writeln!(f, "({:?} -> {:?}, weight: {:?})", e.src, e.dst, e.weight)?;
+            for e in edges {
+                writeln!(
+                    f,
+                    "Edge: {{{:?} -> {:?}, weight: {:?}}},",
+                    e.src, e.dst, e.weight
+                )?;
             }
         }
-        write!(f, "}}",)
+        writeln!(f, "}}",)
     }
 }
 
@@ -74,11 +275,13 @@ mod tests {
     #[test]
     fn value() {
         let mut g = Graph::new(7);
-        g.add_edge(0, 1);
-        g.add_edge(0, 2);
-        g.add_edge(1, 3);
-        g.add_edge(1, 4);
-        g.add_edge(2, 5);
-        g.add_edge(2, 6);
+        g.add_edge(0, 1, 1);
+        g.add_edge(0, 2, 1);
+        g.add_edge(1, 3, 1);
+        g.add_edge(1, 4, 1);
+        g.add_edge(2, 5, 1);
+        g.add_edge(2, 6, 1);
+        assert_eq!(g.is_tree(), true);
+        assert_eq!(g.get_diameter(), Some(4));
     }
 }
