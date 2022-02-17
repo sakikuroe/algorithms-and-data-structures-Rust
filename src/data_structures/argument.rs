1e03bab153beab74f4690a8490b166ac122886fd
sakikuroe <sakikuroe96@gmail.com>
2022-02-17 12:57:12 +0900

    Fix accessibility

--- src/data_structures/argument.rs
+++ src/data_structures/argument.rs
@@ -2,8 +2,8 @@ use std::cmp::Ordering;
 
 #[derive(Clone, Copy, Eq, Debug)]
 pub struct Argument {
-    x: isize,
-    y: isize,
+    pub x: isize,
+    pub y: isize,
 }
 impl Argument {
     pub fn new(x: isize, y: isize) -> Self {
