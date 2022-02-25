ec38bd351ae6cf5df50d49a804f8403ff251cfac
KUROE Saki <sakikuroe96@gmail.com>
2022-02-25 13:15:25 +0900

    Add method

--- src/data_structures/counter.rs
+++ src/data_structures/counter.rs
@@ -9,26 +9,27 @@ impl<T> Counter<T>
 where
     T: Clone + Copy + Eq + Hash + Ord,
 {
-    pub fn new(v: &Vec<T>) -> Self {
-        let mut counter = HashMap::new();
-        for &x in v {
-            *counter.entry(x).or_insert(0) += 1;
-        }
-        Counter {
-            counter,
-        }
+    pub fn new() -> Self {
+        let counter = HashMap::new();
+        Counter { counter }
     }
 
     pub fn add(&mut self, key: T) {
         *self.counter.entry(key).or_insert(0) += 1;
     }
 
-    pub fn values(&mut self) -> Vec<usize> {
+    pub fn keys(&self) -> Vec<T> {
+        self.counter.clone().into_keys().collect()
+    }
+
+    pub fn values(&self) -> Vec<usize> {
         self.counter.clone().into_values().collect()
     }
 
-    pub fn keys(&mut self) -> Vec<T> {
-        self.counter.clone().into_keys().collect()
+    pub fn items(&self) -> Vec<(T, usize)> {
+        let mut res = self.counter.clone().into_iter().collect::<Vec<_>>();
+        res.sort();
+        res
     }
 
     pub fn get(&self, key: T) -> usize {
@@ -40,15 +41,13 @@ where
 
     pub fn get_common(&self) -> Vec<(T, usize)> {
         let mut res = self.counter.clone().into_iter().collect::<Vec<_>>();
-        res.sort_by(
-            |&(t_x, cnt_x), &(t_y, cnt_y)| {
-                if cnt_x == cnt_y {
-                    (t_x).cmp(&t_y)
-                } else {
-                    (cnt_y).cmp(&cnt_x)
-                }
-            },
-        );
+        res.sort_by(|&(t_x, cnt_x), &(t_y, cnt_y)| {
+            if cnt_x == cnt_y {
+                (t_x).cmp(&t_y)
+            } else {
+                (cnt_y).cmp(&cnt_x)
+            }
+        });
         res
     }
 }
@@ -60,7 +59,10 @@ mod tests {
     #[test]
     fn case_usize() {
         let v = vec![3, 1, 4, 1, 5, 9, 2, 6, 5, 3];
-        let counter = Counter::new(&v);
+        let mut counter = Counter::new();
+        for x in v {
+            counter.add(x);
+        }
         assert_eq!(
             vec![(1, 2), (3, 2), (5, 2), (2, 1), (4, 1), (6, 1), (9, 1)],
             counter.get_common()
@@ -69,12 +71,22 @@ mod tests {
 
     #[test]
     fn case_str() {
-        let v = vec!["abc", "def", "ghi", "abc", "abc"];
-        let counter = Counter::new(&v);
-        assert_eq!(vec![("abc", 3), ("def", 1), ("ghi", 1)], counter.get_common());
+        let v = vec!["abc", "def", "ghi", "def", "def"];
+        let mut counter = Counter::new();
+        for x in v {
+            counter.add(x);
+        }
 
-        assert_eq!(counter.get("abc"), 3);
-        assert_eq!(counter.get("def"), 1);
+        assert_eq!(vec![("abc", 1), ("def", 3), ("ghi", 1)], counter.items());
+
+        assert_eq!(
+            vec![("def", 3), ("abc", 1), ("ghi", 1)],
+            counter.get_common()
+        );
+
+        assert_eq!(counter.get("abc"), 1);
+        assert_eq!(counter.get("def"), 3);
+        assert_eq!(counter.get("ghi"), 1);
         assert_eq!(counter.get("jkl"), 0);
     }
 }
