cecb2ae17d639590e2d652021ed477a67ce73fbe
KUROE Saki <sakikuroe96@gmail.com>
2022-03-05 20:44:41 +0900

    Add structs

--- src/algebraic_structures/monoid.rs
+++ src/algebraic_structures/monoid.rs
@@ -23,3 +23,104 @@ impl Identity for MinMonoid {
 impl SemiGroup for MinMonoid {}
 
 impl Monoid for MinMonoid {}
+
+pub struct MaxMonoid;
+
+impl Algebraical for MaxMonoid {
+    type S = usize;
+}
+
+impl Magma for MaxMonoid {
+    fn op(a: Self::S, b: Self::S) -> Self::S {
+        std::cmp::max(a, b)
+    }
+}
+
+impl Identity for MaxMonoid {
+    fn id() -> Self::S {
+        0
+    }
+}
+
+impl SemiGroup for MaxMonoid {}
+
+impl Monoid for MaxMonoid {}
+
+pub struct AddMonoid;
+
+impl Algebraical for AddMonoid {
+    type S = usize;
+}
+
+impl Magma for AddMonoid {
+    fn op(a: Self::S, b: Self::S) -> Self::S {
+        a + b
+    }
+}
+
+impl Identity for AddMonoid {
+    fn id() -> Self::S {
+        0
+    }
+}
+
+impl SemiGroup for AddMonoid {}
+
+impl Monoid for AddMonoid {}
+
+struct RightMonoid;
+
+impl Algebraical for RightMonoid {
+    type S = usize;
+}
+
+impl Magma for RightMonoid {
+    fn op(a: Self::S, b: Self::S) -> Self::S {
+        if b == Self::id() {
+            a
+        } else {
+            b
+        }
+    }
+}
+
+impl Identity for RightMonoid {
+    fn id() -> Self::S {
+        std::usize::MAX
+    }
+}
+
+impl SemiGroup for RightMonoid {}
+
+impl Monoid for RightMonoid {}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Sum {
+    value: usize,
+    len: usize,
+}
+
+pub struct SumMonoid {}
+
+impl Algebraical for SumMonoid {
+    type S = Sum;
+}
+
+impl Magma for SumMonoid {
+    fn op(a: Self::S, b: Self::S) -> Self::S {
+        Sum {
+            value: a.value + b.value,
+            len: a.len + b.len,
+        }
+    }
+}
+
+impl Identity for SumMonoid {
+    fn id() -> Self::S {
+        Sum { value: 0, len: 0 }
+    }
+}
+
+impl SemiGroup for SumMonoid {}
+
+impl Monoid for SumMonoid {}
